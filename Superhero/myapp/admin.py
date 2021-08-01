b1b95b9227357f346f69c0cda421aeb7ef87a48b
kschao <koyschao@icloud.com>
2021-08-01 17:41:37 -0500

    change movie to Superhero

--- Superhero/myapp/admin.py
+++ Superhero/myapp/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Movie
+from .models import Movie, Superhero
 # Register your models here.
 
-admin.site.register(Movie)
+admin.site.register(Superhero)
