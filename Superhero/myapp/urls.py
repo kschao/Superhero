1758cf09377a7e8e0ea0ea7bcd85515026ec5b32
kschao <koyschao@icloud.com>
2021-08-01 17:37:30 -0500

    complete urls

--- Superhero/myapp/urls.py
+++ Superhero/myapp/urls.py
@@ -4,6 +4,9 @@ from . import views
 
 app_name = 'myapp'
 urlpatterns = [
-    path('index', views.index, name='index')
-    path('<int:movie_id>/', views.details, name='detail')
+    path('', views.index, name='index'),
+    path('<int:superhero_id>/', views.detail, name='detail'),
+    path('new/', views.create, name='create_new_superhero'),
+    path('edit/<int:superhero_id>/', views.edit, name='edit'),
+    path('delete/<int:superhero_id>/', views.delete, name='delete')
 ]
