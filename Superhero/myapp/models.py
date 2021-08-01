e03a8a9911349738d5224d900169c88a9cbc4312
kschao <koyschao@icloud.com>
2021-08-01 17:40:45 -0500

    complete models in SUperhero

--- Superhero/myapp/models.py
+++ Superhero/myapp/models.py
@@ -1,12 +1,16 @@
 from django.db import models
 
 # Create your models here.
-class Movie(models.Model):
-    name = models.CharField(max_length=50)
-    genre = models.CharField(max_length=50)
+class Superhero(models.Model):
+    superhero_pic = models.CharField(max_length=100)
+    superheros_names = models.CharField(max_length=50)
+    alter_ego_name = models.CharField(max_length=50)
+    prim_ability = models.CharField(max_length=75)
+    second_ability = models.CharField(max_length=75)
+    catchphrase = models.CharField(max_length=100)
     runtime = models.IntegerField()
     release_date = models.DateField()
 
     def __str__(self):
-        return self.name
+        return self.superheros_names
         
